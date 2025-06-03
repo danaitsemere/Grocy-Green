50988b3a627650ec039c73f61b9c8a3a2cd7c404
Danait Semere <semeregdanait@gmail.com>
2025-06-03 21:59:46 +0300

    added files

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-function addImageWithClass() {
-    const img = document.createElement('img');
-    img.src = 'images/grocylogo-removebg-preview.png';
-    img.classList.add('logo');
-    document.body.appendChild(img);
+
+document.body.style.backgroundColor = "silver";
+const h1Title = document.getElementById("title");
+if (h1Title) {
+    h1Title.style.color = "green";
+}
+const h3Titles = document.querySelectorAll("h3");
+h3Titles.forEach(h3 => {
+    h3.style.textTransform = "uppercase";
+});
+const fruList = document.getElementById("fruList");
+if (fruList) {
+    const newFruit = document.createElement("li");
+    newFruit.textContent = "Apples";
+    fruList.appendChild(newFruit);
 }
 
-
-
-
-
-
-
-
-
+const vegList = document.getElementById("vegList");
+if (vegList) {
+    const newVegetable = document.createElement("li");
+    newVegetable.textContent = "Carrots";
+    vegList.appendChild(newVegetable);
+}
