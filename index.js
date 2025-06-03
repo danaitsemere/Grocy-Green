9e0055ef37000e999ea59f7fef533c1c1fb44407
Danait Semere <semeregdanait@gmail.com>
2025-06-03 10:05:26 +0300

    added files

--- index.js
+++ index.js
@@ -13,31 +13,3 @@ function addImageWithClass() {
 
 
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    
-    const navLinks = document.querySelectorAll('.bottom-nav .nav-links a');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-        
-            navLinks.forEach(nav => nav.classList.remove('active'));
-           
-            event.target.classList.add('active');
-        });
-    });
-
- 
-    const categoryCards = document.querySelectorAll('.product-categories .category-card');
-
-    categoryCards.forEach(card => {
-        card.addEventListener('click', () => {
-            const categoryName = card.querySelector('span').textContent;
-            alert(`You clicked on the "${categoryName}" category!`);
-        });
-    });
-
- 
-});
-
-
